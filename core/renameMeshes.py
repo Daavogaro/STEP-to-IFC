4467bbd880b425f4ab1328fccf5e3e36795b109b
Davide Avogaro <davide.avogaro.2@phd.unipd.it>
2025-11-14 14:21:05 +0100

    Trannsformation of  all of not mesh objects now works

--- core/renameMeshes.py
+++ core/renameMeshes.py
@@ -2,13 +2,13 @@ import bpy
 # This script traverses the hierarchy of a given object, applies transforms to non-mesh objects (in order to do not have problems with mesh substitution in the next steps)
 # and ensures that all mesh objects have unique mesh data.
 def applyTransformsToNonMesh(obj, location, rotation, scale, properties):
+    # If is not a mesh, apply transforms
+    if obj.type != 'MESH':
+        bpy.ops.object.select_all(action='DESELECT')
+        obj.select_set(True)
+        bpy.context.view_layer.objects.active = obj
+        bpy.ops.object.transform_apply(location=location, rotation=rotation, scale=scale, properties=properties)
     for child in obj.children:
-        # If is not a mesh, apply transforms
-        if child.type != 'MESH':
-            bpy.ops.object.select_all(action='DESELECT')
-            child.select_set(True)
-            bpy.context.view_layer.objects.active = child
-            bpy.ops.object.transform_apply(location=location, rotation=rotation, scale=scale, properties=properties)
         # Recurse through children
         applyTransformsToNonMesh(child, location, rotation, scale, properties)
 
